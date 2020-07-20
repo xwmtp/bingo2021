d584789564d1e0d4d15db6e0fb9562e80b789655
xwmtp <xwillmtp@gmail.com>
2020-07-20 21:26:58 +0200

    realigned flags in table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -14,22 +14,18 @@ class Table extends React.Component {
             text-align: center;
         }
 
-        td {
-            text-align: center;
-        }
         tr:nth-child(even) {background-color: #383838;}
 
         td, th {
             padding: 5px 30px;
             border: none;
+            vertical-align: center;
+            text-align: center;
         }
 
         .entrant-name {
             text-align: left
-        }
 
-        .entrant-flag {
-            padding: 5px 0px;
         }
 
     `
@@ -40,39 +36,38 @@ class Table extends React.Component {
         const createTableRow = (rank) => {
             const entrant_data = data['entrants'][rank.toString()]
             return <TableRow
-                rank = {rank}
-                name = {entrant_data['name']}
-                key = {entrant_data['name']}
-                nation = {entrant_data['nationality']}
-                points = {entrant_data['points']}
-                median = {entrant_data['median']}
-                rounds = {entrant_data['rounds']} 
-                dnf = {entrant_data['dnf']}     
+                rank={rank}
+                name={entrant_data['name']}
+                key={entrant_data['name']}
+                nation={entrant_data['nationality']}
+                points={entrant_data['points']}
+                median={entrant_data['median']}
+                rounds={entrant_data['rounds']}
+                dnf={entrant_data['dnf']}
             />
         }
 
         let row_components = []
-        for(let i = 1; i <= rows; i++){
+        for (let i = 1; i <= rows; i++) {
             row_components.push(createTableRow(i))
         }
 
 
-     return (
-         <this.StyledTable>
-             <tbody>
-             <tr>
-                 <th>Rank</th>
-                 <th/>
-                 <th className = 'entrant-name'>Player</th>
-                 <th>Points</th>
-                 <th>Median</th>
-                 <th>Rounds</th>
-                 <th>DNF</th>
-            </tr>
-            {row_components}
-            </tbody>
-         </this.StyledTable>
-     )
+        return (
+            <this.StyledTable>
+                <tbody>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Player</th>
+                        <th>Points</th>
+                        <th>Median</th>
+                        <th>Rounds</th>
+                        <th>DNF</th>
+                    </tr>
+                    {row_components}
+                </tbody>
+            </this.StyledTable>
+        )
     }
 
 }
--- src/components/Tablerow.jsx
+++ src/components/Tablerow.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import styled from "styled-components";
 
 function TableRow(props) {
 
+    const StyledTr = styled.tr`
 
+    td img {
+        height: 15px;
+        padding-right: 10px;
+    }
 
-  return (
-    <tr>
-        <td>{props.rank}</td>
-        <td className='entrant-flag'><img src={`https://raw.githubusercontent.com/xwmtp/bingo2020/master/img/flags/${props.nation}.png`}/></td>
-        <td className='entrant-name'>{props.name}</td>
-        <td>{props.points}</td>
-        <td>{props.median}</td>
-        <td>{props.rounds}</td>
-        <td>{props.dnf}</td>
-    </tr>
-  );
+    .entrant-name {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: left;
+    }
+`
+
+    return (
+        <StyledTr>
+            <td>{props.rank}</td>
+            <td className='entrant-name'><img src={`https://raw.githubusercontent.com/xwmtp/bingo2020/master/img/flags/${props.nation}.png`} /> {props.name}</td>
+            <td>{props.points}</td>
+            <td>{props.median}</td>
+            <td>{props.rounds}</td>
+            <td>{props.dnf}</td>
+        </StyledTr>
+    );
 }
 
 export default TableRow;
