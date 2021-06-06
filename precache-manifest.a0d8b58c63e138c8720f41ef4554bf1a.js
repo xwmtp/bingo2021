f6a0e9273cbe8907e69480bf23677c7fcca826cc
xwmtp <xwillmtp@gmail.com>
2021-06-06 22:17:26 +0200

    Updates

---
+++ precache-manifest.a0d8b58c63e138c8720f41ef4554bf1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559f3748ad714ab4981ef1983b26954a",
+    "url": "/bingo2021/index.html"
+  },
+  {
+    "revision": "471d373a11901c12ceff",
+    "url": "/bingo2021/static/js/2.930aa48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bingo2021/static/js/2.930aa48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30b78b2c305e295f32c",
+    "url": "/bingo2021/static/js/main.7c709a50.chunk.js"
+  },
+  {
+    "revision": "0ff2962a44a999a7b2d7",
+    "url": "/bingo2021/static/js/runtime-main.0a902cd7.js"
+  }
+]);
