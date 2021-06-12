f76609137d0b1f095b7f45f906a52d4aacf323e3
xwmtp <xwillmtp@gmail.com>
2021-06-12 15:50:57 +0200

    Updates

--- precache-manifest.12d9a4eb04ec2db28f0e3872cde79c5b.js
+++ precache-manifest.12d9a4eb04ec2db28f0e3872cde79c5b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4e9f8f08c2b0cab4da94fb77ae35bbe",
+    "revision": "118ed86059214c5d2ca3c204ade952d7",
     "url": "/bingo2021/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bingo2021/static/js/2.930aa48f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a4c905d6a19bede221f",
-    "url": "/bingo2021/static/js/main.3eded180.chunk.js"
+    "revision": "08334b3234d1d035bd79",
+    "url": "/bingo2021/static/js/main.3a10a251.chunk.js"
   },
   {
     "revision": "0ff2962a44a999a7b2d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bingo2021/precache-manifest.518e02baa5d620f4a63b9a523eeef0a0.js"
+  "/bingo2021/precache-manifest.12d9a4eb04ec2db28f0e3872cde79c5b.js"
 );
 
 self.addEventListener('message', (event) => {
