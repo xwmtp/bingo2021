0e14e9c3972e56cd1b1c4da952d8d5abdf7aa2cc
xwmtp <sdstempher@ziggo.nl>
2021-07-05 20:09:48 +0200

    Updates

---
+++ precache-manifest.f65b3c3a56f165b13f656ffcb228f345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3137ab80fcb1d9fcd4cf90b393a5ac",
+    "url": "/bingo2021/index.html"
+  },
+  {
+    "revision": "471d373a11901c12ceff",
+    "url": "/bingo2021/static/js/2.930aa48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bingo2021/static/js/2.930aa48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981bf41c40ec51dd7b5b",
+    "url": "/bingo2021/static/js/main.87d47409.chunk.js"
+  },
+  {
+    "revision": "0ff2962a44a999a7b2d7",
+    "url": "/bingo2021/static/js/runtime-main.0a902cd7.js"
+  }
+]);
