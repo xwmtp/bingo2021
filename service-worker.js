6edfd647b3363a71c0b2b833b4c83a0a35d6928a
xwmtp <xwillmtp@gmail.com>
2021-07-10 12:07:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bingo2021/precache-manifest.26239375ad3d1d53be0f376f7a7e07ce.js"
+  "/bingo2021/precache-manifest.b79a354edede58e12309b4fe3726cf16.js"
 );
 
 self.addEventListener('message', (event) => {
